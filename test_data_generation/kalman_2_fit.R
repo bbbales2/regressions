1985be772ad0c32353619b22e5ce654001175975
Ben <bbbales2@gmail.com>
2021-11-02 20:49:27 -0700

    Added fancier time series model

---
+++ test_data_generation/kalman_2_fit.R
@@ -0,0 +1,19 @@
+library(tidyverse)
+library(cmdstanr)
+
+options(digits = 8)
+
+df = read_csv("test_data_generation/kalman_2.csv")
+mod = cmdstan_model("test_data_generation/kalman_2.stan")
+
+fit = mod$optimize(data = list(
+  N = nrow(df),
+  N_teams = max(df$team1, df$team2),
+  N_years = max(df$year),
+  score_diff = df$score_diff,
+  team1 = df$team1,
+  team2 = df$team2,
+  year = df$year
+))
+
+fit$mle()
---
+++ test_data_generation/kalman_2_generate.R
@@ -0,0 +1,36 @@
+library(tidyverse)
+
+#score_diff ~ normal(skills[team1, year] - skills[team2, year], sigma);
+#skills[team, year] ~ normal(skills[team, lag(year)], 0.5);
+#sigma<lower = 0.0> ~ normal(0, 1.0);
+
+N_teams = 3
+N_years = 5
+N_games_per_year = 100
+
+skills = matrix(0, nrow = N_teams, ncol = N_years)
+for(i in 1:N_teams) {
+  skills[i,] = sin((1:N_years) + i)
+}
+sigma = 3.5
+
+team1s = c()
+team2s = c()
+years = c()
+score_diffs = c()
+for(j in 1:N_years) {
+  for(i in 1:N_games_per_year) {
+    game = sample(1:N_teams, 2)
+    team1 = game[1]
+    team2 = game[2]
+    score_diff = rnorm(1, skills[team1, j] - skills[team2, j], sigma)
+    
+    years = c(years, j)
+    team1s = c(team1s, team1)
+    team2s = c(team2s, team2)
+    score_diffs = c(score_diffs, score_diff)
+  }
+}
+
+df = tibble(score_diff = score_diffs, year = years, team1 = team1s, team2 = team2s)
+write_csv(df, "test_data_generation/kalman_2.csv")
