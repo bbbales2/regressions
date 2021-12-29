bb9d261ba5310ce780dc4d9f7082b2c9ce63b40c
Ben <bbbales2@gmail.com>
2021-12-29 05:46:03 -0800

    Remove comment

--- examples/fakeball/estimate.py
+++ examples/fakeball/estimate.py
@@ -9,14 +9,6 @@ shots_df = pandas.read_csv(os.path.join(fakeball_folder, "shots.csv"))
 with open(os.path.join(mrp_folder, "fakeball.rat")) as f:
     model_string = f.read()
 
-
-# offense0[player] = offense0_z[player] * tau0_offense;
-# defense0[player] = defense0_z[player] * tau0_defense;
-
-# offense0_z[player] ~ normal(0.0, 1.0);
-# defense0_z[player] ~ normal(0.0, 1.0);
-
-
 model = Model(shots_df, model_string=model_string)
 
 print("Running MCMC")
