afe5453dcae7ec5ba5d4f835c3d8fcf52b4e89ed
Dashadower <tttllshin@gmail.com>
2022-02-19 15:26:51 +0900

    Add untracked files

---
+++ rat/nuts.py
@@ -0,0 +1,286 @@
+from collections import defaultdict
+from contextlib import contextmanager
+from dataclasses import dataclass
+import jax
+import math
+import numpy
+import pandas
+import scipy
+import threading
+import time
+from typing import Callable, Set, Tuple, Dict
+from tqdm import tqdm
+
+
+from . import one_draw
+
+
+class Potential:
+    """
+    Potential is a tool for handling gradient calculations on many threads
+    """
+
+    value_and_grad_negative_log_density: Callable[[numpy.array], Tuple[float, numpy.array]]
+    """Gradient of negative log density"""
+
+    potential_results: numpy.array
+    """Pre-allocated array of potential energies"""
+
+    gradient_results: numpy.array
+    """Pre-allocated array of gradients"""
+
+    maximum_threads: int
+    """Maximum number of client threads"""
+
+    local_identity: Dict[int, int]
+    """Remap thread identities to integers"""
+
+    def __init__(self, negative_log_density, maximum_threads, size):
+        """
+        negative_log_density should be a jax-friendly negative log density function that
+        returns a scalar and takes in a vector of length size.
+
+        maximum_threads is the maximum number of threads that will be using this
+        potential for its lifetime (which could be greater than the number using
+        it at any one point).
+
+        maximum_threads and size are used to pre-allocate memory, so they must be
+        specified.
+        """
+        self.value_and_grad_negative_log_density = jax.jit(jax.value_and_grad(negative_log_density))
+
+        self.potential_results = numpy.zeros(maximum_threads, dtype=numpy.float32)
+        self.gradient_results = numpy.zeros((maximum_threads, size), dtype=numpy.float32)
+
+        self.maximum_threads = maximum_threads
+        self.local_identity = {}
+
+    def _get_ident(self):
+        threading_identity = threading.get_ident()
+
+        try:
+            return self.local_identity[threading_identity]
+        except:
+            if len(self.local_identity) == self.maximum_threads:
+                raise Exception("This potential is already at it's maximum number of threads")
+
+            new_local_identity = len(self.local_identity)
+
+            self.local_identity[threading_identity] = new_local_identity
+
+            return new_local_identity
+
+    def value_and_grad(self, q0: numpy.array) -> Tuple[float, numpy.array]:
+        active_thread = self._get_ident()
+
+        device_U, device_gradients = self.value_and_grad_negative_log_density(q0)
+
+        self.potential_results[active_thread] = device_U
+        self.gradient_results[active_thread, :] = device_gradients
+
+        return self.potential_results[active_thread], self.gradient_results[active_thread, :]
+
+
+@dataclass
+class StepsizeAdapter:
+    """
+    Do stepsize adaptation like Stan (https://github.com/stan-dev/stan/blob/develop/src/stan/mcmc/stepsize_adaptation.hpp)
+
+    I've renamed delta from the Stan implementation to target_accept_stat and mu is equal to log(initial_stepsize)
+    """
+
+    target_accept_stat: float
+    initial_stepsize: float
+    gamma: float = 0.05
+    kappa: float = 0.75
+    t0: float = 10.0
+    _counter: int = 0
+    _s_bar: int = 0
+    _x_bar: int = 0
+
+    def adapt(self, accept_stat: float) -> float:
+        self._counter += 1
+
+        accept_stat = min(1.0, accept_stat)
+
+        # Nesterov Dual-Averaging of log(epsilon)
+        eta = 1.0 / (self._counter + self.t0)
+
+        self._s_bar = (1.0 - eta) * self._s_bar + eta * (self.target_accept_stat - accept_stat)
+
+        x = math.log(self.initial_stepsize) - self._s_bar * math.sqrt(self._counter) / self.gamma
+        x_eta = self._counter ** (-self.kappa)
+
+        self._x_bar = (1.0 - x_eta) * self._x_bar + x_eta * x
+
+        return math.exp(x)
+
+    def adapted_stepsize(self):
+        return math.exp(self._x_bar)
+
+
+def one_draw_potential(
+    potential: Potential,
+    rng: numpy.random.Generator,
+    current_draw: numpy.array,
+    stepsize: float,
+    diagonal_inverse_metric: numpy.array,
+    max_treedepth: int = 10,
+):
+    """
+    Generate a draw using Multinomial NUTS (https://arxiv.org/abs/1701.02434 with the original
+    Uturn criteria https://arxiv.org/abs/1111.4246)
+
+    * potential - Potential object representing the unnormalized negative log density of distribution to sample
+    * rng - Instance of numpy.random.Generator for generating rngs
+    * current_draw - current draw
+    * stepsize - leapfrog stepsize
+    * diagonal_inverse_metric - Diagonal of the inverse metric (usually diagonal of covariance of draws)
+    * max_treedepth - max treedepth
+
+    Return a tuple containing containing next draw, the acceptance probability statistic, and how many leapfrog
+    steps were used to compute the draw
+    """
+    return one_draw.do_it(potential.value_and_grad, rng, current_draw, diagonal_inverse_metric, stepsize, max_treedepth)
+
+
+def warmup(
+    potential: Potential,
+    rng: numpy.random.Generator,
+    initial_draw: numpy.array,
+    initial_stepsize: float = 1.0,
+    initial_diagonal_inverse_metric: numpy.array = None,
+    max_treedepth: int = 10,
+    target_accept_stat: float = 0.8,
+    stage_1_size: int = 100,
+    stage_2_size: int = 850,
+    stage_2_window_count: int = 4,
+    stage_3_size: int = 50,
+):
+    """
+    Do warmup for a NUTS sampler given a potential (function proportional to the negative log
+    density of the distribution we want to sample), a random number generator, and an
+    initial position in sample space
+
+    Return a tuple (draw, stepsize, diagonal_inverse_metric) where draw is a suitable
+    draw for starting an MCMC calculation, stepsize is a stepsize that will hopefully
+    allow the sampler to hit the target acceptance rate, and diagonal_inverse_metric
+    is a scaling of parameter space to make the NUTS sampler reasonably efficient
+
+    Though stepsize and diagonal_inverse_metric are estimated in the process, optional
+    initial values can be provided.
+
+    target_accept_stat is the target acceptance rate for the sampler.
+
+    Adaptation is broken into three stages, each using a specified number of MCMC draws to
+    accomplish their task.
+
+    Stage 1 adapts a stepsize and tries to move the sampler close to where it will equilibrate.
+
+    Stage 2 adapts a metric iteratively. It is further broken into stages (how many times the
+    metric is recomputed). The number of stages here can be specified with stage_2_window_count.
+
+    Stage 3 adapts a final timestep after an appropriate metric has been found.
+    """
+
+    size = initial_draw.shape[0]
+    stepsize = initial_stepsize
+
+    if initial_diagonal_inverse_metric is None:
+        diagonal_inverse_metric = numpy.ones(size)
+    else:
+        diagonal_inverse_metric = initial_diagonal_inverse_metric
+        assert diagonal_inverse_metric.shape[0] == size and diagonal_inverse_metric.shape[1] == size
+
+    with tqdm(total=stage_1_size + stage_2_size + stage_3_size, desc="Moving from initial condition") as progress_bar:
+        # Find an initial stepsize that is large enough we are under our target accept rate
+        while True:
+            next_draw, accept_stat, steps = one_draw_potential(potential, rng, initial_draw, stepsize, diagonal_inverse_metric)
+            if accept_stat < target_accept_stat:
+                break
+            stepsize = stepsize * 2.0
+
+        # Back off until we hit our target accept rate
+        while True:
+            stepsize = stepsize / 2.0
+            next_draw, accept_stat, steps = one_draw_potential(potential, rng, initial_draw, stepsize, diagonal_inverse_metric)
+            if accept_stat > target_accept_stat:
+                break
+
+        # Stage 1, leave initial conditions in the dust, adapting stepsize
+        current_draw = initial_draw
+        stepsize_adapter = StepsizeAdapter(target_accept_stat, stepsize)
+        for i in range(stage_1_size):
+            current_draw, accept_stat, steps = one_draw_potential(potential, rng, current_draw, stepsize, diagonal_inverse_metric)
+            stepsize = stepsize_adapter.adapt(accept_stat)
+            progress_bar.update()
+            progress_bar.set_description(f"Moving from initial condition [leapfrogs {steps}]")
+
+        # Stage 2, estimate diagonal of covariance in windows of increasing size
+        stage_2_windows = []
+
+        # Compute window size such that we have a sequence of stage_2_window_count
+        # windows that sequentially double in size and in total take less than
+        # or equal to stage_2_size draws. In math terms that is something like:
+        # window_size + 2 * window_size + 4 * window_size + ... <= stage_2_size
+        window_size = math.floor(stage_2_size / (2**stage_2_window_count - 1))
+
+        for i in range(stage_2_window_count - 1):
+            stage_2_windows.append(window_size * 2**i)
+
+        # The last window is whatever is left
+        stage_2_windows.append(stage_2_size - sum(stage_2_windows))
+
+        for current_window_size in stage_2_windows:
+            stepsize_adapter = StepsizeAdapter(target_accept_stat, stepsize)
+            window_draws = numpy.zeros((current_window_size, size))
+            for i in range(current_window_size):
+                current_draw, accept_stat, steps = one_draw_potential(potential, rng, current_draw, stepsize, diagonal_inverse_metric)
+                window_draws[i] = current_draw
+                stepsize = stepsize_adapter.adapt(accept_stat)
+                progress_bar.update()
+                progress_bar.set_description(f"Building a metric [leapfrogs {steps}]")
+            new_diagonal_inverse_metric = numpy.var(window_draws, axis=0)
+            max_scale_change = max(diagonal_inverse_metric / new_diagonal_inverse_metric)
+            stepsize = stepsize * max_scale_change
+            diagonal_inverse_metric = new_diagonal_inverse_metric
+
+        # Stage 3, fine tune timestep
+        stepsize_adapter = StepsizeAdapter(target_accept_stat, stepsize)
+        for i in range(stage_3_size):
+            current_draw, accept_stat, steps = one_draw_potential(potential, rng, current_draw, stepsize, diagonal_inverse_metric)
+            stepsize = stepsize_adapter.adapt(accept_stat)
+            progress_bar.update()
+            progress_bar.set_description(f"Finalizing timestep [leapfrogs {steps}]")
+
+    return current_draw, stepsize_adapter.adapted_stepsize(), diagonal_inverse_metric
+
+
+def sample(
+    potential: Potential,
+    rng: numpy.random.Generator,
+    initial_draw: numpy.array,
+    stepsize: float,
+    diagonal_inverse_metric: numpy.array,
+    num_draws: int,
+    max_treedepth: int = 10,
+):
+    """
+    Run the NUTS sampler given a potential (function proportional to negative log density of distribution
+    we want to sample), a random number generator, an initial draw, a stepsize, a diagonal inverse metric,
+    a desired number of draws, and the maximum treedepth to be used in the calculation.
+
+    The initial draw, stepsize, and diagonal inverse metric should be computed with warmup in most situations.
+    """
+    size = initial_draw.shape[0]
+    draws = numpy.zeros((num_draws, size))
+
+    with tqdm(total=num_draws, desc="Sampling") as progress_bar:
+        current_draw = initial_draw
+        for i in range(num_draws):
+            current_draw, accept_stat, steps = one_draw_potential(potential, rng, current_draw, stepsize, diagonal_inverse_metric)
+            draws[i, :] = current_draw
+            progress_bar.update()
+            progress_bar.set_description(f"Sampling [leapfrogs {steps}]")
+
+    return draws
---
+++ src/lib.rs
@@ -0,0 +1,350 @@
+
+use pyo3::prelude::*;
+use pyo3::prelude::{PyResult, Python, PyObject};
+use pyo3::types::IntoPyDict;
+use pyo3::conversion::ToPyObject;
+use ndarray::*;
+use numpy::{IntoPyArray, PyReadonlyArray1};
+use std::iter::FromIterator;
+use numpy::ToPyArray;
+use pyo3::{pymodule, types::PyModule};
+
+fn uturn(q_plus: &ArrayView1<f64>, q_minus: &ArrayView1<f64>, p_plus: &ArrayView1<f64>, p_minus: &ArrayView1<f64>) -> bool {
+    let uturn_forward = (q_plus - q_minus).dot(p_plus) <= 0.0;
+    let uturn_backward = -(q_minus - q_plus).dot(p_minus) <= 0.0;
+
+    uturn_forward && uturn_backward
+}
+
+fn kinetic_energy(p : &Array1<f64>, diag_m_inv : &Array1<f64>) -> f64 {
+    0.5 * (p * diag_m_inv).dot(p)
+}
+
+fn value_and_grad(py: Python, py_value_and_grad: &PyObject, q : &Array1<f64>) -> (f64, Array1<f64>) {
+    let res = py_value_and_grad.call1(py, (q.to_pyarray(py),)).unwrap().extract(py);
+    let (u, grad) : (f64, Vec<f64>) = match res {
+        Ok(v) => v,
+        Err(_) => panic!("Error evaluating gradient (calling value_and_grad from Rust)")
+    };
+    (u, Array1::from_vec(grad))
+}
+
+fn log_sum_exp(x : &Vec<f64>) -> f64 {
+    let y = x.iter().fold(f64::NEG_INFINITY, |a, &b| f64::max(a, b));
+    y + x.iter().map(|xv| (xv - y).exp()).sum::<f64>().ln()
+}
+  
+fn softmax(x : &Vec<f64>) -> Vec<f64>{
+    Vec::from_iter(x.iter().map(|xv| (xv - log_sum_exp(x)).exp()))
+}
+
+#[pyfunction]
+fn do_it<'py>(
+    py: Python<'py>,
+    py_value_and_grad : PyObject,
+    py_rng : PyObject,
+    py_q0 : PyReadonlyArray1<f64>,
+    py_diagonal_inverse_metric : PyReadonlyArray1<f64>,
+    stepsize : f64,
+    max_treedepth : usize
+) -> PyResult<Vec<PyObject>> {
+    let q0 = py_q0.as_array().to_owned();
+    let diag_m_inv = py_diagonal_inverse_metric.as_array().to_owned();
+
+    let size = q0.len();
+    let kwargs = [("size", size)].into_py_dict(py);
+    let z : Array1<f64> = Array1::from_vec(py_rng.call_method(py, "normal", (), Some(kwargs)).unwrap().extract(py)?);
+    let kwargs = [("low", 0), ("high", 2), ("size", max_treedepth + 1)].into_py_dict(py);
+    let directions : Vec<i32> = py_rng.call_method(py, "integers", (), Some(kwargs)).unwrap().extract(py)?;
+    let kwargs = [("size", max_treedepth)].into_py_dict(py);
+    let choice_draws : Vec<f64> = py_rng.call_method(py, "random", (), Some(kwargs)).unwrap().extract(py)?;
+    let pick_draws : Vec<f64> = py_rng.call_method(py, "random", (), Some(kwargs)).unwrap().extract(py)?;
+
+    // Allocate return values
+    let mut q_ret = Array1::zeros(size);
+    let mut accept_stat : Option<f64> = None;
+    let mut total_leapfrogs_taken = 0;
+
+    // Apparently using as for the conversion is kinda bad cause it can mess up
+    let max_leapfrogs : usize = 2_usize.pow(max_treedepth as u32);
+
+    // To compute the U-turn conditions, we need to save our positions/momentums
+    // Technically we only need to save the new subtree we computed, but to avoid
+    // a bunch of mallocs we just save the whole trajectory
+
+    // qs stores our positions
+    let mut qs = Array2::zeros((max_leapfrogs, size));
+    // ps stores our momentums
+    let mut ps = Array2::zeros((max_leapfrogs, size));
+
+    py.allow_threads(||{
+        let p0 = z / diag_m_inv.mapv(|x : f64| x.sqrt());//Vec::from_iter(z.iter().zip(diag_m_inv.iter()).map(|(z, m_inv)| z / m_inv.sqrt()));
+
+        let (u0, _) = Python::with_gil(|py| {
+            value_and_grad(py, &py_value_and_grad, &q0)
+        });
+        let h0 = kinetic_energy(&p0, &diag_m_inv) + u0;
+
+        // directions is a length max_treedepth vector that maps each treedepth
+        //  to an integration direction (left or right)
+        let directions : Vec<i32> = Vec::from_iter(directions.iter().map(|d| if *d == 0 {-1} else {1}));
+            
+        // Compute the position of the initial condition in the trajectory
+        let mut i_first = 0;
+        for (depth, &direction) in directions[1..].iter().enumerate() {
+            if direction < 0 {
+                i_first = i_first + (1 << depth);
+            }
+        }
+
+        // log_pi defined in section A.2.3 of https://arxiv.org/abs/1701.02434
+        let mut log_pi = vec![0_f64; max_leapfrogs];
+
+        qs.row_mut(i_first).assign(&q0);
+        ps.row_mut(i_first).assign(&p0);
+        log_pi[i_first] = -h0;
+        
+        let mut divergence = false;
+        // i_left and i_right are indices that track the leftmost and rightmost
+        //  states of the integrated trajectory
+        let mut i_left = i_first;
+        let mut i_right = i_first;
+        // log_sum_pi_old is the log of the sum of the pis (of log_pi) for the
+        //  tree processed so far
+        let mut log_sum_pi_old = log_pi[i_first];
+        // i_old will be the sample chosen (the sample from T(z' | told) in section
+        //  A.3.1 of https://arxiv.org/abs/1701.02434)
+        let mut i_old = i_first;
+        // For trees of increasing treedepth
+        
+        let mut checks = Vec::new();
+        checks.reserve(2 << max_treedepth);
+        for depth in 1..(max_treedepth + 1) {
+            // Figure out what leapfrog steps we need to compute. If integrating in the
+            //  positive direction update the index that points at the right side of the
+            //  trajectory. If integrating in the negative direction update the index pointing
+            //  to the left side of the trajectory.
+            let max_leapfrog_steps = 1 << (depth - 1);
+            let depth_steps = if directions[depth] < 0 {
+                Vec::from_iter(((i_left - max_leapfrog_steps)..i_left).rev())
+            } else {
+                Vec::from_iter((i_right + 1)..(i_right + 1 + max_leapfrog_steps))
+            };
+            
+            let i_prev;
+            if directions[depth] < 0 {
+                i_prev = i_left;
+                i_left = *depth_steps.last().unwrap();
+            } else {
+                i_prev = i_right;
+                i_right = *depth_steps.last().unwrap();
+            }
+
+            // What we're doing here is generating a trajectory composed of a number of leapfrog states.
+            // We apply a set of comparisons on this trajectory that can be organized to look like a binary tree.
+            // Sometimes I say trajectory instead of tree. Each piece of the tree in the comparison corresponds
+            //  to a subset of the trajectory. When I say trajectory I'm referring to a piece of the trajectory that
+            //  also corresponds to some sorta subtree in the comparisons.
+            //
+            // This is probably confusing but what I want to communicate is trajectory and tree are very related
+            //  but maybe technically not the same thing.
+            //
+            // Detect U-turns in newly integrated subtree
+            let mut uturn_detected_new_tree = false;
+
+            // Starts and ends are relative because they point to the ith leapfrog step in a sub-trajectory
+            //  of size 2^(depth - 1) (this is max_leapfrog_steps) which needs to be mapped to the global
+            // index of qs (which is defined in the range 1:2^max_treedepth)
+            //
+            // We're assuming at this point that depth_steps is in sorted order
+            //
+            // The sort is necessary because depth_steps is sorted in order of leapfrog steps taken
+            //  which might be backwards in time (so decreasing instead of increasing)
+            //
+            // This sort is important because we need to keep track of what is left and what is right
+            //  in the trajectory so that we do the right comparisons
+            if depth > 1 {
+                // Each new subtree is twice as wide as the previous. For each half of the new
+                // subtree, we need to do all the checks as the previous subtree, plus one
+                // additional end-to-end check
+                let div_length = max_leapfrog_steps >> 1;
+                // Add the start of left to start of right check
+                checks.push((0, div_length));
+                // Copy and shift the previous checks
+                for i in 0..checks.len() - 1 {
+                    let (x, y) = checks[i];
+                    checks.push((x + div_length, y + div_length));
+                }
+                // Add the end of left to end of right check
+                checks.push((div_length - 1, 2 * div_length - 1));
+                // And the additional start of left to end of right check
+                checks.push((0, 2 * div_length - 1));
+            }
+
+            // Sort the checks so that we only need to check them in order
+            checks.sort_by_key(|&(_, end)| end);
+
+            let dt = stepsize * directions[depth] as f64;
+
+            let mut q = qs.row(i_prev).to_owned();
+            let mut p = ps.row(i_prev).to_owned();
+
+            let (_, qgrad) = Python::with_gil(|py| {
+                value_and_grad(py, &py_value_and_grad, &q)
+            });
+            let mut grad = qgrad;
+
+            // Initialize pointer into checks list
+            let mut check_i = 0;
+
+            let mut leapfrogs_taken = 0;
+            for i in depth_steps.iter() {
+                // leapfrog step
+                leapfrogs_taken += 1;
+
+                let p_half = p - (dt / 2.0) * grad;
+                q = q + dt * &diag_m_inv * &p_half;
+                let (u, qgrad) = Python::with_gil(|py| {
+                    value_and_grad(py, &py_value_and_grad, &q)
+                });
+                grad = qgrad;
+                p = p_half - (dt / 2.0) * &grad;
+
+                let k = kinetic_energy(&p, &diag_m_inv);
+                let h = k + u;
+
+                qs.row_mut(*i).assign(&q);
+                ps.row_mut(*i).assign(&p);
+
+                log_pi[*i] = -h;
+
+                while check_i < checks.len() {
+                    let (left, right) = checks[check_i];
+                    if right < leapfrogs_taken {
+                        let (start, end) = if directions[depth] > 0 { (left, right) } else { (depth_steps.len() - left - 1, depth_steps.len() - right - 1) };
+                        let start_i = depth_steps[start];
+                        let end_i = depth_steps[end];
+    
+                        let is_uturn = uturn(
+                            &qs.row(end_i),
+                            &qs.row(start_i),
+                            &ps.row(end_i),
+                            &ps.row(start_i),
+                        );
+    
+                        if is_uturn {
+                            uturn_detected_new_tree = true;
+                            break;
+                        }
+
+                        check_i += 1;
+                    } else {
+                        break;
+                    }
+                }
+
+                if uturn_detected_new_tree {
+                    break;
+                }
+            }
+
+            total_leapfrogs_taken += leapfrogs_taken;
+
+            // We need to know whether we terminated the trajectory cause of a uturn or we
+            //  hit the max trajectory length
+            // Merging the two trees requires one more uturn check from the overall left to right states
+            let uturn_detected = uturn_detected_new_tree || uturn(&qs.row(i_right), &qs.row(i_left), &ps.row(i_right), &ps.row(i_left));
+
+            // Accept statistic from ordinary HMC
+            // Only compute the accept probability for the steps done
+            let mut p_tree_accept = 0.0;
+            let log_pi_steps = Vec::from_iter((0..leapfrogs_taken).map(|i| log_pi[depth_steps[i]]));
+            for i in 0..leapfrogs_taken {
+                let energy_loss = h0 + log_pi_steps[i];
+                p_tree_accept += energy_loss.exp().min(1.0) / leapfrogs_taken as f64;
+
+                if energy_loss.is_nan() || energy_loss.abs() > 1000.0 {
+                    divergence = true;
+                }
+            }
+
+            // Divergence
+            if divergence {
+                if accept_stat.is_none() {
+                    accept_stat = Some(0.0);
+                }
+
+                break;
+            }
+
+            if uturn_detected {
+                // If we u-turn on the first step, grab something for the accept_stat
+                if accept_stat.is_none() {
+                    accept_stat = Some(p_tree_accept);
+                }
+
+                break
+            }
+
+            accept_stat = match accept_stat {
+                None => Some(p_tree_accept),
+                Some(accept_stat) => {
+                    let weight = leapfrogs_taken as f64 / (leapfrogs_taken + depth_steps.len() - 1) as f64;
+                    Some(accept_stat * (1.0 - weight) + p_tree_accept * weight)
+                }
+            };
+
+            // log of the sum of pi (A.3.1 of https://arxiv.org/abs/1701.02434) of the new subtree
+            let log_sum_pi_new = log_sum_exp(&log_pi_steps);
+
+            // sample from the new subtree according to the equation in A.2.1 in https://arxiv.org/abs/1701.02434
+            //  (near the end of that section)
+            let i_new = if depth > 1 {
+                let probs = softmax(&log_pi_steps);
+
+                let choice_draw = choice_draws[depth - 1];
+
+                let mut total_prob = 0.0_f64;
+                let mut depth_step : usize = 0;
+                for i in 0..probs.len() {
+                    let next_prob = total_prob + probs[i];
+                    depth_step = depth_steps[i];
+                    if choice_draw >= total_prob && choice_draw < next_prob {
+                        break;
+                    } else {
+                        total_prob = next_prob;
+                    }
+                }
+                depth_step
+            } else {
+                depth_steps[0]
+            };
+
+            // Pick between the samples generated from the new and old subtrees using the biased progressive sampling in
+            //  A.3.2 of https://arxiv.org/abs/1701.02434
+            let p_new = (log_sum_pi_new - log_sum_pi_old).exp().min(1.0);
+            if pick_draws[depth - 1] < p_new {
+                i_old = i_new
+            }
+
+            // Update log of sum of pi of overall tree
+            log_sum_pi_old = log_sum_exp(&vec![log_sum_pi_old, log_sum_pi_new]);
+
+        }
+
+        // Get the final sample
+        q_ret.assign(&qs.row(i_old));
+    });
+
+    Ok(vec![
+        q_ret.into_pyarray(py).to_object(py),
+        accept_stat.unwrap().to_object(py),
+        total_leapfrogs_taken.to_object(py)
+    ])
+}
+
+#[pymodule]
+fn one_draw(_: Python<'_>, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(do_it, m)?)?;
+    Ok(())
+}
