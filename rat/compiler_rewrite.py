f821262887dad4a07dc10d2eb791b50d95b0f1a1
Dashadower <tttllshin@gmail.com>
2022-03-21 02:20:41 +0900

    Update symbol table and dataframe generation

--- rat/compiler_rewrite.py
+++ rat/compiler_rewrite.py
@@ -10,7 +10,7 @@ from typing import Iterable, List, Dict, Set, Tuple, Union
 from enum import Enum
 from dataclasses import dataclass, field
 import pprint
-import warnings
+import itertools
 
 import pandas as pd
 
@@ -18,7 +18,7 @@ from . import ast
 
 
 class CompileError(Exception):
-    def __init__(self, message, code_string: str, line_num: int = -1, column_num: int = -1):
+    def __init__(self, message, code_string: str = "", line_num: int = -1, column_num: int = -1):
         code_string = code_string.split("\n")[line_num] if code_string else ""
         exception_message = f"An error occured while compiling the following line({line_num}:{column_num}):\n{code_string}\n{' ' * column_num + '^'}\n{message}"
         super().__init__(exception_message)
@@ -34,7 +34,8 @@ class VariableType(Enum):
 class TableRecord:
     name: str
     variable_type: VariableType
-    subscripts: List[Set[Tuple[str, int]]]  # tuple of (subscript_name, shift_amount)
+    subscripts: Set[Tuple[str]]  # tuple of (subscript_name, subscript_name, ...)
+    subscript_length: int = field(default=0)
     base_df: pd.DataFrame = field(default=None, kw_only=True)
 
 
@@ -42,39 +43,49 @@ class SymbolTable:
     def __init__(self):
         self.symbol_dict: Dict[str, TableRecord] = {}
 
-    def upsert(self, variable_name: str, variable_type: VariableType = None, subscripts: List[Set[Tuple[str, int]]] = None):
+    def upsert(self, variable_name: str, variable_type: VariableType = None, subscripts: Set[Tuple[str]] = None):
         if variable_name in self.symbol_dict:
             record = self.symbol_dict[variable_name]
             record.variable_type = variable_type
             if not subscripts:
                 return
-            if len(record.subscripts) != len(subscripts) and len(record.subscripts) > 0:
-                raise ValueError(
-                    f"Internal error - symbol table update failed. Variable {variable_name} in table has {len(record.subscripts)} subscripts, but update request has {len(subscripts)}"
-                )
+            if record.subscript_length > 0:
+                for subscript_tuple in subscripts:
+                    if len(subscript_tuple) != record.subscript_length:
+                        raise ValueError(
+                            f"Internal error - symbol table update failed. Variable {variable_name} in table has {len(record.subscripts)} subscripts, but update request has {len(subscripts)}"
+                        )
 
-            for index, subscript_set in enumerate(subscripts):
-                try:
-                    record.subscripts[index] |= subscript_set
-                except IndexError:
-                    record.subscripts.append(subscript_set)
+            if subscripts:
+                record.subscript_length = len(tuple(subscripts)[0])
+                record.subscripts |= subscripts
 
         else:
-            self.symbol_dict[variable_name] = TableRecord(variable_name, variable_type, subscripts)
+            self.symbol_dict[variable_name] = TableRecord(variable_name, variable_type, subscripts if subscripts else set(), len(tuple(subscripts)[0]) if subscripts else 0)
 
     def lookup(self, variable_name: str):
         return self.symbol_dict[variable_name]
 
     def build_dataframes(self, data_df: pd.DataFrame):
-        dataframes = {}
         for variable_name, record in self.symbol_dict.items():
             if record.variable_type == VariableType.DATA:
                 continue
 
-            base_df = pd.DataFrame({f"subscript__{key}": [] for key in range(len(record.subscripts))})
+            if record.subscript_length == 0:
+                continue
 
+            subscript_length = record.subscript_length
+            base_df = pd.DataFrame()
+            for subscript_tuple in record.subscripts:
+                try:
+                    df = data_df.loc[:, subscript_tuple]
+                except KeyError as e:
+                    raise CompileError(f"Internal Error - dataframe build failed. Could not index one or more columns in {subscript_tuple} from the data dataframe.") from e
 
+                df.columns = tuple([f"subscript__{x}" for x in range(subscript_length)])
+                base_df = pd.concat([base_df, df]).drop_duplicates().sort_values(list(df.columns)).reset_index(drop=True)
 
+            record.base_df = base_df
 
     def __str__(self):
         return pprint.pformat(self.symbol_dict)
@@ -190,7 +201,7 @@ class Compiler:
                         if primary_variable.subscript:
                             for index, subscript_column in enumerate(primary_variable.subscript.names):
                                 if subscript_column.name not in self.data_df_columns:
-                                    subscript_aliases[subscript_column.name] = primary_variable_record.subscripts[index]
+                                    subscript_aliases[subscript_column.name] = {subscript_tuple[index] for subscript_tuple in primary_variable_record.subscripts}
 
                 case _:
                     msg = f"Expected a parameter or data primary variable but got type {primary_variable.__class__.__name__}"
@@ -198,6 +209,8 @@ class Compiler:
 
             if isinstance(top_expr, ast.Assignment):
                 self.symbol_table.upsert(top_expr.lhs.name, VariableType.ASSIGNED_PARAM)
+
+            print(primary_variable_key, subscript_aliases)
 
             for param in ast.search_tree(top_expr, ast.Param):
                 param_key = param.get_key()
@@ -213,24 +226,23 @@ class Compiler:
                 except KeyError:
                     var_type = VariableType.DATA if param_key in self.data_df_columns else VariableType.PARAM
                 n_subscripts = len(param.subscript.names)
-                subscript_list = []
+                subscript_list = [[] for _ in range(n_subscripts)]
                 for index in range(n_subscripts):
                     subscript_column = param.subscript.names[index]
                     subscript_name = subscript_column.name
-                    subscript_shift = param.subscript.shifts[index].value
 
                     if subscript_name in self.data_df_columns:
-                        subscript_list.append({(subscript_name, subscript_shift)})
+                        subscript_list[index].append(subscript_name)
                     elif subscript_name in subscript_aliases:
-                        subscript_list.append({s for s in subscript_aliases[subscript_name]})
+                        subscript_list[index].extend(subscript_aliases[subscript_name])
                     else:
                         raise CompileError(f"Unknown subscript name '{subscript_name}'", self.model_code_string, subscript_column.line_index, subscript_column.column_index)
 
-                self.symbol_table.upsert(param_key, var_type, subscript_list)
+                unrolled_subscripts = [tuple(x) for x in itertools.product(*subscript_list)]
 
-        print(self.symbol_table)
+                self.symbol_table.upsert(param_key, var_type, set(unrolled_subscripts))
 
-    def build_base_df(self):
+    def generate_code(self):
         pass
 
 
@@ -238,3 +250,5 @@ class Compiler:
         self._identify_primary_symbols()
 
         self.build_symbol_table()
+
+        self.symbol_table.build_dataframes(self.data_df)
