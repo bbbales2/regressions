4b5a4740650f8c4fa564d19e7c27857cdcebf12e
Dashadower <tttllshin@gmail.com>
2022-11-01 13:11:21 +0900

    Allow expressions on the LHS of sample statements.

--- rat/parser.py
+++ rat/parser.py
@@ -16,16 +16,16 @@ import sys
 from tatsu.buffering import Buffer
 from tatsu.parsing import Parser
 from tatsu.parsing import tatsumasu
-from tatsu.parsing import leftrec, nomemo, isname  # noqa
+from tatsu.parsing import leftrec, nomemo, isname # noqa
 from tatsu.infos import ParserConfig
 from tatsu.util import re, generic_main  # noqa
 
 
 KEYWORDS = {
-    "let",
-    "in",
-    "end",
-    "fun",
+    'let',
+    'in',
+    'end',
+    'fun',
 }  # type: ignore
 
 
@@ -37,9 +37,9 @@ class RatBuffer(Buffer):
             whitespace=None,
             nameguard=None,
             comments_re=None,
-            eol_comments_re="#.*?$",
+            eol_comments_re='#.*?$',
             ignorecase=False,
-            namechars="",
+            namechars='',
             parseinfo=True,
         )
         config = config.replace(**settings)
@@ -54,135 +54,207 @@ class RatParser(Parser):
             whitespace=None,
             nameguard=None,
             comments_re=None,
-            eol_comments_re="#.*?$",
+            eol_comments_re='#.*?$',
             ignorecase=False,
-            namechars="",
+            namechars='',
             parseinfo=True,
             keywords=KEYWORDS,
-            start="start",
+            start='start',
         )
         config = config.replace(**settings)
         super().__init__(config=config)
 
-    @tatsumasu("Program")
+    @tatsumasu('Program')
+    @nomemo
     def _start_(self):  # noqa
+
         def block0():
             self._statement_()
-            self.add_last_node_to_name("statements")
-
+            self.add_last_node_to_name('statements')
         self._positive_closure(block0)
         self._check_eof()
 
-        self._define([], ["statements"])
-
-    @tatsumasu("Statement")
-    def _statement_(self):  # noqa
-        self._factor_()
-        self.name_last_node("left")
-        self._cut()
-        with self._group():
-            with self._choice():
-                with self._option():
-                    self._token("~")
-                with self._option():
-                    self._token("=")
-                self._error("expecting one of: " "'=' '~'")
-        self.name_last_node("op")
-        self._cut()
-        self._expression_()
-        self.name_last_node("right")
-        self._token(";")
-
-        self._define(["left", "op", "right"], [])
+        self._define(
+            [],
+            ['statements']
+        )
 
     @tatsumasu()
+    @nomemo
+    def _statement_(self):  # noqa
+        with self._choice():
+            with self._option():
+                self._sample_statement_()
+            with self._option():
+                self._assignment_statement_()
+            self._error(
+                'expecting one of: '
+                '<assignment_statement> <factor>'
+                '<sample_statement> <variable>'
+            )
+
+    @tatsumasu('Statement')
+    @nomemo
+    def _sample_statement_(self):  # noqa
+        self._factor_()
+        self.name_last_node('left')
+        with self._group():
+            self._token('~')
+        self.name_last_node('op')
+        self._cut()
+        self._expression_()
+        self.name_last_node('right')
+        self._token(';')
+
+        self._define(
+            ['left', 'op', 'right'],
+            []
+        )
+
+    @tatsumasu('Statement')
+    def _assignment_statement_(self):  # noqa
+        self._variable_()
+        self.name_last_node('left')
+        with self._group():
+            self._token('=')
+        self.name_last_node('op')
+        self._cut()
+        self._expression_()
+        self.name_last_node('right')
+        self._token(';')
+
+        self._define(
+            ['left', 'op', 'right'],
+            []
+        )
+
+    @tatsumasu()
+    @nomemo
     def _expression_(self):  # noqa
         with self._choice():
             with self._option():
                 self._logical_()
             with self._option():
                 self._addition_expression_()
-            self._error("expecting one of: " "<addition> <addition_expression>" "<logical> <multiplication_expression>")
+            self._error(
+                'expecting one of: '
+                '<addition> <addition_expression>'
+                '<logical> <multiplication_expression>'
+            )
 
-    @tatsumasu("Binary")
+    @tatsumasu('Binary')
+    @nomemo
     def _logical_(self):  # noqa
         self._addition_expression_()
-        self.name_last_node("left")
+        self.name_last_node('left')
         with self._group():
             with self._choice():
                 with self._option():
-                    self._token("==")
+                    self._token('==')
                 with self._option():
-                    self._token("<=")
+                    self._token('<=')
                 with self._option():
-                    self._token(">=")
+                    self._token('>=')
                 with self._option():
-                    self._token("<")
+                    self._token('<')
                 with self._option():
-                    self._token(">")
-                self._error("expecting one of: " "'<' '<=' '==' '>' '>='")
-        self.name_last_node("op")
+                    self._token('>')
+                self._error(
+                    'expecting one of: '
+                    "'<' '<=' '==' '>' '>='"
+                )
+        self.name_last_node('op')
         self._cut()
         self._expression_()
-        self.name_last_node("right")
+        self.name_last_node('right')
 
-        self._define(["left", "op", "right"], [])
+        self._define(
+            ['left', 'op', 'right'],
+            []
+        )
 
     @tatsumasu()
+    @nomemo
     def _addition_expression_(self):  # noqa
         with self._choice():
             with self._option():
                 self._addition_()
             with self._option():
                 self._multiplication_expression_()
-            self._error("expecting one of: " "<addition> <factor> <multiplication>" "<multiplication_expression>")
+            self._error(
+                'expecting one of: '
+                '<addition> <factor> <multiplication>'
+                '<multiplication_expression>'
+            )
 
-    @tatsumasu("Binary")
+    @tatsumasu('Binary')
+    @nomemo
     def _addition_(self):  # noqa
         self._multiplication_expression_()
-        self.name_last_node("left")
+        self.name_last_node('left')
         with self._group():
             with self._choice():
                 with self._option():
-                    self._token("+")
+                    self._token('+')
                 with self._option():
-                    self._token("-")
-                self._error("expecting one of: " "'+' '-'")
-        self.name_last_node("op")
+                    self._token('-')
+                self._error(
+                    'expecting one of: '
+                    "'+' '-'"
+                )
+        self.name_last_node('op')
         self._cut()
         self._addition_expression_()
-        self.name_last_node("right")
+        self.name_last_node('right')
 
-        self._define(["left", "op", "right"], [])
+        self._define(
+            ['left', 'op', 'right'],
+            []
+        )
 
     @tatsumasu()
+    @nomemo
     def _multiplication_expression_(self):  # noqa
         with self._choice():
             with self._option():
                 self._multiplication_()
             with self._option():
                 self._factor_()
-            self._error("expecting one of: " "<factor> <function_call> <ifelse>" "<literal> <multiplication>" "<subexpression> <variable>")
+            self._error(
+                'expecting one of: '
+                '<addition_expression> <expression>'
+                '<factor> <function_call> <ifelse>'
+                '<literal> <logical> <multiplication>'
+                '<subexpression> <variable>'
+            )
 
-    @tatsumasu("Binary")
+    @tatsumasu('Binary')
+    @nomemo
     def _multiplication_(self):  # noqa
         self._factor_()
-        self.name_last_node("left")
+        self.name_last_node('left')
         with self._group():
             with self._choice():
                 with self._option():
-                    self._token("*")
+                    self._token('*')
                 with self._option():
-                    self._token("/")
-                self._error("expecting one of: " "'*' '/'")
-        self.name_last_node("op")
+                    self._token('/')
+                self._error(
+                    'expecting one of: '
+                    "'*' '/'"
+                )
+        self.name_last_node('op')
         self._cut()
         self._multiplication_expression_()
-        self.name_last_node("right")
+        self.name_last_node('right')
 
-        self._define(["left", "op", "right"], [])
+        self._define(
+            ['left', 'op', 'right'],
+            []
+        )
 
     @tatsumasu()
+    @leftrec
     def _factor_(self):  # noqa
         with self._choice():
             with self._option():
@@ -192,118 +264,149 @@ class RatParser(Parser):
             with self._option():
                 self._subexpression_()
             with self._option():
+                self._expression_()
+            with self._option():
                 self._variable_()
             with self._option():
                 self._literal_()
             self._error(
-                "expecting one of: " "'(' 'ifelse' <function_call>" "<identifier> <integer> <literal> <real>" "<subexpression> <variable>"
+                'expecting one of: '
+                "'(' 'ifelse' <addition>"
+                '<addition_expression> <expression>'
+                '<function_call> <identifier> <integer>'
+                '<literal> <logical>'
+                '<multiplication_expression> <real>'
+                '<subexpression> <variable>'
             )
 
-    @tatsumasu("IfElse")
+    @tatsumasu('IfElse')
     def _ifelse_(self):  # noqa
-        self._token("ifelse")
-        self._token("(")
+        self._token('ifelse')
+        self._token('(')
         self._cut()
         self._expression_()
-        self.name_last_node("predicate")
-        self._token(",")
+        self.name_last_node('predicate')
+        self._token(',')
         self._expression_()
-        self.name_last_node("left")
-        self._token(",")
+        self.name_last_node('left')
+        self._token(',')
         self._expression_()
-        self.name_last_node("right")
-        self._token(")")
+        self.name_last_node('right')
+        self._token(')')
 
-        self._define(["left", "predicate", "right"], [])
+        self._define(
+            ['left', 'predicate', 'right'],
+            []
+        )
 
-    @tatsumasu("FunctionCall")
+    @tatsumasu('FunctionCall')
     def _function_call_(self):  # noqa
         self._identifier_()
-        self.name_last_node("name")
-        self._token("(")
+        self.name_last_node('name')
+        self._token('(')
         with self._optional():
             self._arglist_()
-            self.name_last_node("arglist")
-        self._token(")")
+            self.name_last_node('arglist')
+        self._token(')')
 
-        self._define(["arglist", "name"], [])
+        self._define(
+            ['arglist', 'name'],
+            []
+        )
 
-    @tatsumasu("Variable")
+    @tatsumasu('Variable')
     def _variable_(self):  # noqa
         self._identifier_()
-        self.name_last_node("name")
+        self.name_last_node('name')
         with self._optional():
             self._constraints_()
-            self.name_last_node("constraints")
+            self.name_last_node('constraints')
         with self._optional():
-            self._token("[")
+            self._token('[')
             self._arglist_()
-            self.name_last_node("arglist")
-            self._token("]")
+            self.name_last_node('arglist')
+            self._token(']')
 
-            self._define(["arglist"], [])
+            self._define(
+                ['arglist'],
+                []
+            )
         with self._optional():
             self._token("'")
-            self.name_last_node("prime")
+            self.name_last_node('prime')
 
-        self._define(["arglist", "constraints", "name", "prime"], [])
+        self._define(
+            ['arglist', 'constraints', 'name', 'prime'],
+            []
+        )
 
-    @tatsumasu("Constraints")
+    @tatsumasu('Constraints')
     def _constraints_(self):  # noqa
-        self._token("<")
+        self._token('<')
         self._constraint_()
-        self.name_last_node("left")
+        self.name_last_node('left')
         self._cut()
         with self._optional():
-            self._token(",")
+            self._token(',')
             self._constraint_()
-            self.name_last_node("right")
+            self.name_last_node('right')
 
-            self._define(["right"], [])
-        self._token(">")
+            self._define(
+                ['right'],
+                []
+            )
+        self._token('>')
 
-        self._define(["left", "right"], [])
+        self._define(
+            ['left', 'right'],
+            []
+        )
 
     @tatsumasu()
     def _constraint_(self):  # noqa
         with self._group():
             with self._choice():
                 with self._option():
-                    self._token("lower")
+                    self._token('lower')
                 with self._option():
-                    self._token("upper")
-                self._error("expecting one of: " "'lower' 'upper'")
-        self.name_last_node("name")
+                    self._token('upper')
+                self._error(
+                    'expecting one of: '
+                    "'lower' 'upper'"
+                )
+        self.name_last_node('name')
         self._cut()
-        self._token("=")
+        self._token('=')
         self._cut()
         self._literal_()
-        self.name_last_node("value")
+        self.name_last_node('value')
 
-        self._define(["name", "value"], [])
+        self._define(
+            ['name', 'value'],
+            []
+        )
 
     @tatsumasu()
     def _arglist_(self):  # noqa
         self._expression_()
-        self.add_last_node_to_name("@")
+        self.add_last_node_to_name('@')
 
         def block1():
-            self._token(",")
+            self._token(',')
             self._cut()
             self._expression_()
-            self.add_last_node_to_name("@")
-
+            self.add_last_node_to_name('@')
         self._closure(block1)
 
     @tatsumasu()
     def _subexpression_(self):  # noqa
-        self._token("(")
+        self._token('(')
         self._cut()
         self._expression_()
-        self.name_last_node("@")
-        self._token(")")
+        self.name_last_node('@')
+        self._token(')')
 
-    @tatsumasu("Literal")
+    @tatsumasu('Literal')
     def _literal_(self):  # noqa
         with self._group():
             with self._choice():
@@ -311,26 +414,34 @@ class RatParser(Parser):
                     self._real_()
                 with self._option():
                     self._integer_()
-                self._error("expecting one of: " "<integer> <real> [-]?[0-9]+\\.[0-9]*" "[-]?[0-9]+\\.[0-9]*e[-+]?[0-9]+ [-]?\\d+")
-        self.name_last_node("value")
+                self._error(
+                    'expecting one of: '
+                    '<integer> <real> [-]?[0-9]+\\.[0-9]*'
+                    '[-]?[0-9]+\\.[0-9]*e[-+]?[0-9]+ [-]?\\d+'
+                )
+        self.name_last_node('value')
 
-    @tatsumasu("int")
+    @tatsumasu('int')
     def _integer_(self):  # noqa
-        self._pattern("[-]?\\d+")
+        self._pattern('[-]?\\d+')
 
-    @tatsumasu("float")
+    @tatsumasu('float')
     def _real_(self):  # noqa
         with self._choice():
             with self._option():
-                self._pattern("[-]?[0-9]+\\.[0-9]*e[-+]?[0-9]+")
+                self._pattern('[-]?[0-9]+\\.[0-9]*e[-+]?[0-9]+')
             with self._option():
-                self._pattern("[-]?[0-9]+\\.[0-9]*")
-            self._error("expecting one of: " "[-]?[0-9]+\\.[0-9]*" "[-]?[0-9]+\\.[0-9]*e[-+]?[0-9]+")
+                self._pattern('[-]?[0-9]+\\.[0-9]*')
+            self._error(
+                'expecting one of: '
+                '[-]?[0-9]+\\.[0-9]*'
+                '[-]?[0-9]+\\.[0-9]*e[-+]?[0-9]+'
+            )
 
-    @tatsumasu("str")
+    @tatsumasu('str')
     @isname
     def _identifier_(self):  # noqa
-        self._pattern("[a-zA-Z_][a-zA-Z0-9_]*")
+        self._pattern('[a-zA-Z_][a-zA-Z0-9_]*')
 
 
 class RatSemantics:
@@ -338,6 +449,12 @@ class RatSemantics:
         return ast
 
     def statement(self, ast):  # noqa
+        return ast
+
+    def sample_statement(self, ast):  # noqa
+        return ast
+
+    def assignment_statement(self, ast):  # noqa
         return ast
 
     def expression(self, ast):  # noqa
@@ -396,19 +513,23 @@ class RatSemantics:
 
 
 def main(filename, **kwargs):
-    if not filename or filename == "-":
+    if not filename or filename == '-':
         text = sys.stdin.read()
     else:
         with open(filename) as f:
             text = f.read()
     parser = RatParser()
-    return parser.parse(text, filename=filename, **kwargs)
+    return parser.parse(
+        text,
+        filename=filename,
+        **kwargs
+    )
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     import json
     from tatsu.util import asjson
 
-    ast = generic_main(main, RatParser, name="Rat")
+    ast = generic_main(main, RatParser, name='Rat')
     data = asjson(ast)
     print(json.dumps(data, indent=2))
